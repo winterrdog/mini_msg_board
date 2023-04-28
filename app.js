9aa0fe940969ef9f7928f37668b2f7c1ed89d8fd
winterrdog <winterrdog@keemail.me>
2023-04-28 16:46:00 +0300

    removed `users` router

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -18,6 +17,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "assets")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 module.exports = app;
